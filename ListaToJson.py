59671f990f2fd5eb1e7365c257030f90cf4a1144
unknown <zemariamateus@gmail.com>
2021-12-15 14:58:17 +0000

    finished project

--- ListaToJson.py
+++ ListaToJson.py
@@ -9,22 +9,64 @@ day-month....
 import json
 
 
-f = open("ListaFeriados", "r")
+def Feriados():
+    f = open("ListaFeriados", "r")
 
-myDict = {}
+    myDict = {}
 
-for line in f.readlines():
-    myvalue = line.split("\n")[0].split("-")
+    for line in f.readlines():
+        myvalue = line.split("\n")[0].split("-")
 
-    if myvalue[1] in myDict:
-        myDict[myvalue[1]].append(myvalue[0])
-    else:
-        myDict[myvalue[1]] = [myvalue[0]]
+        if myvalue[1] in myDict:
+            myDict[myvalue[1]].append(myvalue[0])
+        else:
+            myDict[myvalue[1]] = [myvalue[0]]
 
-print(myDict)
+    print(myDict)
 
-with open('data.json', 'w') as fp:
-    json.dump(myDict, fp)
+    with open('data.json', 'w') as fp:
+        json.dump(myDict, fp)
 
 
-f.close()
+    f.close()
+
+def Timetables():
+    """
+    get the timetable in a raw format from the file and convert it to json
+    """
+    station = "Carcavelos"
+    f = open(station, "r")
+
+    myDict = {}
+    flag = "none"
+    for line in f.readlines():
+        print("this is the line ", line)
+        if line == "normal:\n":
+            flag = "n"
+            myDict[flag] = {}
+        elif line == "feriados:\n":
+            flag = "f"
+            myDict[flag] = {}
+            print("f flag was added")
+        else:
+            timeList = line.split(" ")
+            for time in timeList:
+
+                hour = time.split(":")[0]
+                minute = time.split(":")[1]
+                if "\n" in minute:
+                    minute = minute[:-1]
+                if hour not in myDict[flag]:
+                    myDict[flag][hour] = [minute]
+                
+                else:
+                    myDict[flag][hour].append(minute)
+    
+    with open('time.json', 'w') as fp:
+        json.dump(myDict, fp)
+
+
+
+    f.close()
+
+Timetables()
--- server.py
+++ server.py
@@ -28,19 +28,64 @@ def IsHoliday(dtime):
 
 
 
+def DealTime(time, isholiday):
+    """
+    time is in hh:mm
+    Receive a time and a holiday flag and return how many minutes until the next train
+    """
+    global timetable
+    
+    hour = time.split(":")[0]
+    minute = time.split(":")[1]
+    print(f"This is the hour {hour}:{minute}")
 
+    if isholiday:
+        flag = "f"
+    else:
+        flag = "n"
 
-@app.route('/helloesp')
-def helloHandler():
-    return str(IsWeekend(datetime.date.today()))
+    option1 = timetable[flag][hour]
+    print(f"This is the {option1}")
+    minOp1 = ""
+    for temp in option1:
+        if int(minute) <= int(temp):
+            minOp1 = temp
+            break
+    
+    if minOp1 == "":
+        hour = (int(hour) + 1) %24
+        # print(f"This is the closest --> {hour}:{timetable[flag][hour][0]}")
+        return (int(minute)-int(timetable[flag][hour][0]))%60
+    else:
+        # print(f"This is the closest --> {hour}:{minOp1}")
+        return int(minOp1)-int(minute)
+
+def GetClosest():
+    """
+    Returns the closest train using all the variables above
+    """
+    mydate = datetime.datetime.now()
+    mytime = f"{mydate.hour}:{mydate.minute}"
+
+    if IsHoliday(mydate) or IsWeekend(mydate):
+        return DealTime(mytime, True)
+    
+    return DealTime(mytime, False)
+
 
 # Opening JSON file
 f = open('data.json') 
 holiday = json.load(f)
 f.close()
- 
-print(IsWeekend(datetime.date.today()))
-print(IsHoliday(datetime.date.today()))
+f = open('time.json') 
+timetable = json.load(f)
+f.close()
+
+
+@app.route('/carcavelos-lisboa')
+def helloHandler():
+    return str(GetClosest())
+
 
 app.run(host='0.0.0.0', port= 8090)
 
