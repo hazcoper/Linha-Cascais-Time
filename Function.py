f69206e6a826ff1396dbf1e458caf5eb53152e47
unknown <zemariamateus@gmail.com>
2021-12-16 00:52:24 +0000

    cleaned the code

--- Function.py
+++ Function.py
@@ -37,10 +37,7 @@ def DealTime(time, isholiday, timetable):
         flag = "n"
 
     if hour not in timetable[flag] or str((int(hour) + 1)%24) not in timetable[flag]:
-        print(hour not in timetable[flag])
-        print(timetable[flag])
-        print(hour, type(hour))
-        print(timetable[flag][hour])
+
         return -100
     
     option1 = timetable[flag][hour]
--- server.py
+++ server.py
@@ -17,11 +17,11 @@ f = open('time.json')
 timetable = json.load(f)
 f.close()
 
-print(GetClosest(timetable, holidaylist, datetime.datetime(2021, 12, 20, 20, 15, 10)))
+# print(GetClosest(timetable, holidaylist, datetime.datetime(2021, 12, 20, 20, 15, 10)))
 
-# @app.route('/carcavelos-lisboa')
-# def helloHandler():
-#     return GetClosest(timetable,holidaylist)
+@app.route('/carcavelos-lisboa')
+def helloHandler():
+    return GetClosest(timetable,holidaylist)
 
 
-# app.run(host='0.0.0.0', port= 8090)
+app.run(host='0.0.0.0', port= 8090)
